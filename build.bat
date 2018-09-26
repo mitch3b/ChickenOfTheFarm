13a64634665e4a782420582f849d4027452e383d
Phillip Grimsrud <phillip.grimsrud@gmail.com>
2018-09-26 00:01:41 -0600

    Add a 2nd stage Add an end of level portal modified the top of level 1 and corresponding collision create defines for PPU_CTRL and PPU_MASK added stage loading added a fade between stages fix a build script bug

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ C:\Users\pgrimsrud\NES\cc65\bin\cc65 -Oi ChickenOfTheFarm.c -o ChickenOfTheFarm.
 C:\Users\pgrimsrud\NES\cc65\bin\ca65 ChickenOfTheFarm.asm
 C:\Users\pgrimsrud\NES\cc65\bin\ca65 main.asm
 C:\Users\pgrimsrud\NES\cc65\bin\ca65 asm4c.s
-C:\Users\pgrimsrud\NES\cc65\bin\ld65 -C nes.cfg -o ChickenOfTheFarm.nes main.o test.o --mapfile ChickenOfTheFarm.map asm4c.o "C:\Users\pgrimsrud\NES\cc65\lib\nes.lib"
+C:\Users\pgrimsrud\NES\cc65\bin\ld65 -C nes.cfg -o ChickenOfTheFarm.nes main.o ChickenOfTheFarm.o --mapfile ChickenOfTheFarm.map asm4c.o "C:\Users\pgrimsrud\NES\cc65\lib\nes.lib"
