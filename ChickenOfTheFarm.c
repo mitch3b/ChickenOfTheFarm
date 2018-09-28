3d89ec97da4e954dd6d2bc630a183834b9c3cd47
Phillip Grimsrud <phillip.grimsrud@gmail.com>
2018-09-28 00:28:29 -0600

    Add defines and macros for colors Add a state machine for the game state Fix? the vblank counter again Fix the starting y position for the frog Add an ending state, but note that I reused the start screen Add a couple of init functions Move the main loop stuff to state machine functions so the main loop for each state can be unique

--- ChickenOfTheFarm.c
+++ ChickenOfTheFarm.c
@@ -1,3 +1,6 @@
+//
+// DEFINES
+//
 
 // Controller bits
 #define BUTTON_A 0x80
@@ -9,11 +12,128 @@
 #define BUTTON_LEFT 0x02
 #define BUTTON_RIGHT 0x01
 
+// Colors
+#define            BLACK         0x0D
+#define            BLACK_2       0x0E
+#define            BLACK_3       0x0F
+#define            BLACK_4       0x1D
+#define            BLACK_5       0x1E
+#define            BLACK_6       0x1F
+#define            BLACK_7       0x2E
+#define            BLACK_8       0x2F
+#define            BLACK_9       0x3E
+#define            BLACK_10      0x3F
+#define       DARK_GRAY          0x00
+#define       DARK_GRAY_2        0x2D
+#define            GRAY          0x10
+#define            GRAY_2        0x3D
+#define      LIGHT_GRAY          0x20
+#define            WHITE         0x30
+#define       DARK_GRAY_BLUE     0x01
+#define            GRAY_BLUE     0x11
+#define      LIGHT_GRAY_BLUE     0x21
+#define VERY_LIGHT_GRAY_BLUE     0x31
+#define       DARK_BLUE          0x02
+#define            BLUE          0x12
+#define      LIGHT_BLUE          0x22
+#define VERY_LIGHT_BLUE          0x32
+#define       DARK_PURPLE        0x03
+#define            PURPLE        0x13
+#define      LIGHT_PURPLE        0x23
+#define VERY_LIGHT_PURPLE        0x33
+#define       DARK_PINK          0x04
+#define            PINK          0x14
+#define      LIGHT_PINK          0x24
+#define VERY_LIGHT_PINK          0x34
+#define       DARK_FUCHSIA       0x05
+#define            FUCHSIA       0x15
+#define      LIGHT_FUCHSIA       0x25
+#define VERY_LIGHT_FUCHSIA       0x35
+#define       DARK_RED           0x06
+#define            RED           0x16
+#define      LIGHT_RED           0x26
+#define VERY_LIGHT_RED           0x36
+#define       DARK_ORANGE        0x07
+#define            ORANGE        0x17
+#define      LIGHT_ORANGE        0x27
+#define VERY_LIGHT_ORANGE        0x37
+#define       DARK_TAN           0x08
+#define            TAN           0x18
+#define      LIGHT_TAN           0x28
+#define VERY_LIGHT_TAN           0x38
+#define       DARK_GREEN         0x09
+#define            GREEN         0x19
+#define      LIGHT_GREEN         0x29
+#define VERY_LIGHT_GREEN         0x39
+#define       DARK_LIME_GREEN    0x0A
+#define            LIME_GREEN    0x1A
+#define      LIGHT_LIME_GREEN    0x2A
+#define VERY_LIGHT_LIME_GREEN    0x3A
+#define       DARK_SEAFOAM_GREEN 0x0B
+#define            SEAFOAM_GREEN 0x1B
+#define      LIGHT_SEAFOAM_GREEN 0x2B
+#define VERY_LIGHT_SEAFOAM_GREEN 0x3B
+#define       DARK_CYAN          0x0C
+#define            CYAN          0x1C
+#define      LIGHT_CYAN          0x2C
+#define VERY_LIGHT_CYAN          0x3C
+
+#define BACKGROUND0_0            0x00
+#define BACKGROUND0_1            0x01
+#define BACKGROUND0_2            0x02
+#define BACKGROUND0_3            0x03
+#define BACKGROUND1_0            0x04
+#define BACKGROUND1_1            0x05
+#define BACKGROUND1_2            0x06
+#define BACKGROUND1_3            0x07
+#define BACKGROUND2_0            0x08
+#define BACKGROUND2_1            0x09
+#define BACKGROUND2_2            0x0A
+#define BACKGROUND2_3            0x0B
+#define BACKGROUND3_0            0x0C
+#define BACKGROUND3_1            0x0D
+#define BACKGROUND3_2            0x0E
+#define BACKGROUND3_3            0x0F
+#define SPRITE0_0                0x10
+#define SPRITE0_1                0x11
+#define SPRITE0_2                0x12
+#define SPRITE0_3                0x13
+#define SPRITE1_0                0x14
+#define SPRITE1_1                0x15
+#define SPRITE1_2                0x16
+#define SPRITE1_3                0x17
+#define SPRITE2_0                0x18
+#define SPRITE2_1                0x19
+#define SPRITE2_2                0x1A
+#define SPRITE2_3                0x1B
+#define SPRITE3_0                0x1C
+#define SPRITE3_1                0x1D
+#define SPRITE3_2                0x1E
+#define SPRITE3_3                0x1F
+
 // Registers
 #define PPU_CTRL    *((unsigned char*)0x2000)
 #define PPU_MASK    *((unsigned char*)0x2001)
 #define PPU_ADDRESS *((unsigned char*)0x2006)
 #define PPU_DATA    *((unsigned char*)0x2007)
+
+#define SET_COLOR( index, color )  PPU_ADDRESS = 0x3F; PPU_ADDRESS = index; PPU_DATA = color
+
+//
+// TYPEDEFS
+//
+
+typedef enum _GameState_t
+{
+    TITLE_SCREEN_STATE = 0,
+    DEMO_STATE,
+    GAME_RUNNING_STATE,
+    ENDING_STATE,
+} GameState_t;
+
+//
+// GLOBALS
+//
 
 #pragma bss-name (push, "OAM")
 unsigned char sprites[256];
@@ -49,6 +169,7 @@ static unsigned int  gStage;
 static unsigned int  gCounter;
 static unsigned int  gHealth;
 static unsigned int  gIframes;
+static GameState_t   gGameState;
 
 extern void pMusicInit(unsigned char);
 extern void pMusicPlay(void);
@@ -98,12 +219,10 @@ void vblank(void)
 
 void vblank_counter(void)
 {
-    do
+    for( i = 0; i < gCounter; i++ )
     {
         vblank();
-        gCounter = gCounter - 1;
     }
-    while( gCounter != 0 );
 }
 
 void ppuinit(void)
@@ -266,77 +385,36 @@ void loadCollisionFromNametables(void)
 void palattes(void)
 {
     // Background 0
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x00;
-    PPU_DATA =    0x0E;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x01;
-    PPU_DATA    = 0x00;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x02;
-    PPU_DATA    = 0x10;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x03;
-    PPU_DATA    = 0x30;
+    SET_COLOR(BACKGROUND0_0, BLACK);
+    SET_COLOR(BACKGROUND0_1, DARK_GRAY);
+    SET_COLOR(BACKGROUND0_2, GRAY);
+    SET_COLOR(BACKGROUND0_3, WHITE);
 
     // Background 1
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x04;
-    PPU_DATA =    0x0E;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x05;
-    PPU_DATA    = 0x00;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x06;
-    PPU_DATA    = 0x10;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x07;
-    PPU_DATA    = 0x20;
-
+    SET_COLOR(BACKGROUND1_0, BLACK);
+    SET_COLOR(BACKGROUND1_1, DARK_GRAY);
+    SET_COLOR(BACKGROUND1_2, GRAY);
+    SET_COLOR(BACKGROUND1_3, WHITE);
 
     // Sprite 0
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x11;
-    PPU_DATA    = 0x0A;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x12;
-    PPU_DATA    = 0x1A;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x13;
-    PPU_DATA    = 0x30;
+    SET_COLOR(SPRITE0_1, DARK_GREEN);
+    SET_COLOR(SPRITE0_2, GREEN);
+    SET_COLOR(SPRITE0_3, WHITE);
 
     // Sprite 1
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x15;
-    PPU_DATA    = 0x11;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x16;
-    PPU_DATA    = 0x02;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x17;
-    PPU_DATA    = 0x27;
+    SET_COLOR(SPRITE1_1, GRAY_BLUE);
+    SET_COLOR(SPRITE1_2, DARK_BLUE);
+    SET_COLOR(SPRITE1_3, LIGHT_ORANGE);
 
     // Sprite 2
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x19;
-    PPU_DATA    = 0x06;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x1A;
-    PPU_DATA    = 0x16;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x1B;
-    PPU_DATA    = 0x30;
+    SET_COLOR(SPRITE2_1, DARK_RED);
+    SET_COLOR(SPRITE2_2, RED);
+    SET_COLOR(SPRITE2_3, WHITE);
 
     // Sprite 3
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x1D;
-    PPU_DATA    = 0x16;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x1E;
-    PPU_DATA    = 0x30;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x1F;
-    PPU_DATA    = 0x30;
+    SET_COLOR(SPRITE3_1, RED);
+    SET_COLOR(SPRITE3_2, WHITE);
+    SET_COLOR(SPRITE3_3, WHITE);
 }
 
 //void ppu_copy( unsigned char* addr, unsigned int len )
@@ -357,19 +435,10 @@ void fade_out(void)
     gCounter = 20;
     vblank_counter();
 
-    // Background 0
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x00;
-    PPU_DATA    = 0x0E;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x01;
-    PPU_DATA    = 0x0E;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x02;
-    PPU_DATA    = 0x00;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x03;
-    PPU_DATA    = 0x10;
+    SET_COLOR(BACKGROUND0_0, BLACK);
+    SET_COLOR(BACKGROUND0_1, BLACK);
+    SET_COLOR(BACKGROUND0_2, DARK_GRAY);
+    SET_COLOR(BACKGROUND0_3, GRAY);
 
     PPU_CTRL = 0x84 + gYNametable;
     set_scroll();
@@ -377,13 +446,8 @@ void fade_out(void)
     gCounter = 20;
     vblank_counter();
 
-    // Background 0
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x02;
-    PPU_DATA    = 0x0E;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x03;
-    PPU_DATA    = 0x00;
+    SET_COLOR(BACKGROUND0_2, BLACK);
+    SET_COLOR(BACKGROUND0_3, DARK_GRAY);
 
     PPU_CTRL = 0x84 + gYNametable;
     set_scroll();
@@ -391,10 +455,7 @@ void fade_out(void)
     gCounter = 20;
     vblank_counter();
 
-    // Background 0
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x03;
-    PPU_DATA    = 0x0E;
+    SET_COLOR(BACKGROUND0_3, BLACK);
 
     PPU_CTRL = 0x84 + gYNametable;
     set_scroll();
@@ -409,10 +470,7 @@ void fade_out(void)
 
 void fade_in(void)
 {
-    // Background 0
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x03;
-    PPU_DATA    = 0x00;
+    SET_COLOR(BACKGROUND0_3, DARK_GRAY);
 
     PPU_CTRL = 0x84 + gYNametable;
     PPU_MASK = 0x0E;
@@ -421,13 +479,8 @@ void fade_in(void)
     gCounter = 20;
     vblank_counter();
 
-    // Background 0
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x02;
-    PPU_DATA    = 0x00;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x03;
-    PPU_DATA    = 0x10;
+    SET_COLOR(BACKGROUND0_2, DARK_GRAY);
+    SET_COLOR(BACKGROUND0_3, GRAY);
 
     PPU_CTRL = 0x84 + gYNametable;
     set_scroll();
@@ -435,16 +488,9 @@ void fade_in(void)
     gCounter = 20;
     vblank_counter();
 
-    // Background 0
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x01;
-    PPU_DATA    = 0x00;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x02;
-    PPU_DATA    = 0x10;
-    PPU_ADDRESS = 0x3F;
-    PPU_ADDRESS = 0x03;
-    PPU_DATA    = 0x30;
+    SET_COLOR(BACKGROUND0_1, DARK_GRAY);
+    SET_COLOR(BACKGROUND0_2, GRAY);
+    SET_COLOR(BACKGROUND0_3, WHITE);
 
     PPU_CTRL = 0x84 + gYNametable;
     if( gStage != 0 )
@@ -551,7 +597,7 @@ void setup_sprites(void)
     sprites[15] = 0x58;
 
     gX = 0x10;
-    gY = 0xD0;
+    gY = 0xCF;
     gVelocity = 0;
     gVelocityDirection = 0;
     gSpeed = 0;
@@ -802,7 +848,7 @@ void load_stage(void)
     vblank();
 
     gX = 0x10;
-    gY = 0xD0;
+    gY = 0xCF;
     gYNametable = 2;
     gVelocity = 0;
     gVelocityDirection = 0;
@@ -851,14 +897,22 @@ void load_stage(void)
     fade_in();
 }
 
+void next_stage(void)
+{
+    switch( gStage )
+    {
+        case 2:
+            gStage = 0;
+            gGameState = ENDING_STATE;
+            break;
+
+        default:
+            gStage++;
+    }
+}
+
 void do_physics(void)
 {
-    if( (gController1 & BUTTON_START) != 0 )
-    {
-        gStage = 1;
-        load_stage();
-    }
-
     // 0-15 Frog 4 sprites
     // 16-23 Bird
     if( sprites[16] != gY )
@@ -1279,11 +1333,8 @@ void do_physics(void)
 
     if( gYNametable == 0 && gY == 0x0F && gX == 0xE0 )
     {
-        if( gStage == 1 )
-        {
-            gStage = 2;
-            load_stage();
-        }
+        next_stage();
+        load_stage();
     }
 
     if( sprites[16] == gY && sprites[19] == gX && gIframes == 0)
@@ -1299,6 +1350,7 @@ void do_physics(void)
             {
                 gHealth = 8;
                 gStage = 0;
+                gGameState = TITLE_SCREEN_STATE;
                 load_stage();
                 draw_health();
             }
@@ -1314,8 +1366,85 @@ void do_physics(void)
     }
 }
 
+void init_globals(void)
+{
+    gGameState = TITLE_SCREEN_STATE;
+}
+
+void init_game_state(void)
+{
+    gX = 0x10;
+    gY = 0xCF;
+    gYNametable = 2;
+    gVelocity = 0;
+    gVelocityDirection = 0;
+    gXScroll = 0;
+    gYScroll = 0;
+    gSpeed = 0;
+    gSpeedDirection = 1;
+    gJumping = 1;
+    gBounceCounter = 0;
+    gHealth = 8;
+}
+
+void game_running_sm(void)
+{
+    vblank();
+
+    input_poll();
+
+    update_sprites();
+
+    dma_sprites();
+
+    // set bits [1:0] to 0 for nametable
+    PPU_CTRL = 0x84 + gYNametable;
+    set_scroll();
+
+    do_physics();
+}
+
+void title_screen_sm(void)
+{
+    vblank();
+
+    input_poll();
+
+    // set bits [1:0] to 0 for nametable
+    PPU_CTRL = 0x84 + gYNametable;
+    set_scroll();
+
+    if( (gController1 & BUTTON_START) != 0 )
+    {
+        gStage = 1;
+        gGameState = GAME_RUNNING_STATE;
+        init_game_state();
+        load_stage();
+    }
+}
+
+void end_screen_sm(void)
+{
+    vblank();
+
+    input_poll();
+
+    // set bits [1:0] to 0 for nametable
+    PPU_CTRL = 0x84 + gYNametable;
+    set_scroll();
+
+    if( (gController1 & BUTTON_START) != 0 )
+    {
+        gStage = 0;
+        gGameState = TITLE_SCREEN_STATE;
+        load_stage();
+    }
+}
+
 void main(void)
 {
+    init_globals();
+
     gCounter = 5;
     vblank_counter();
 
@@ -1346,18 +1475,22 @@ void main(void)
 
     while(1)
     {
-        vblank();
+        switch( gGameState )
+        {
+            case GAME_RUNNING_STATE:
+                game_running_sm();
+                break;
 
-        input_poll();
+            case ENDING_STATE:
+                end_screen_sm();
+                break;
 
-        update_sprites();
+            case DEMO_STATE:
+            case TITLE_SCREEN_STATE:
+            default:
+                title_screen_sm();
+                break;
+        }
 
-        dma_sprites();
-
-        // set bits [1:0] to 0 for nametable
-        PPU_CTRL = 0x84 + gYNametable;
-        set_scroll();
-
-        do_physics();
     }
 }
